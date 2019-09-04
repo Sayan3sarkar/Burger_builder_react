3a8bbbeb931d0d1a4995e0658730779b3f3fc699
Sayan3sarkar <sayansarkar333@gmail.com>
2019-09-04 14:27:48 +0530

    Error handling added

--- burger/src/components/UI/Modal/Modal.js
+++ burger/src/components/UI/Modal/Modal.js
@@ -6,13 +6,13 @@ import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
 
-    shouldComponentUpdate ( nextProps, nextState ) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+    shouldComponentUpdate (nextProps, nextState) {
+        return ((nextProps.show !== this.props.show) || (nextProps.children !== this.props.children));
     }
 
-    componentWillUpdate () {
+    /*componentWillUpdate () {
         console.log('[Modal] WillUpdate');
-    }
+    }*/
 
     render () {
         return (
--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
 import Spinner from '../../components/UI/Spinner/Spinner'
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler'
 
 const INGREDIENT_PRICES = {
     cheese: 30,
@@ -143,4 +144,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder,axios);
