81cfd0e839c34802daf683a3ebbd615bb9fe7dac
Sayan3sarkar <sayansarkar333@gmail.com>
2019-09-04 16:28:35 +0530

    Progress as of 04/09/2019

--- burger/src/components/UI/Modal/Modal.js
+++ burger/src/components/UI/Modal/Modal.js
@@ -6,13 +6,13 @@ import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
 
-    shouldComponentUpdate (nextProps, nextState) {
-        return ((nextProps.show !== this.props.show) || (nextProps.children !== this.props.children));
+    shouldComponentUpdate ( nextProps, nextState ) {
+        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
     }
 
-    /*componentWillUpdate () {
-        console.log('[Modal] WillUpdate');
-    }*/
+    componentDidUpdate () {
+        //console.log('[Modal] WillUpdate');
+    }
 
     render () {
         return (
