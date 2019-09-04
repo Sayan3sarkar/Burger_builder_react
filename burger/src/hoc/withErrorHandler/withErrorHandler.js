c2d8b7ca36c5298dac8d4b54183b441502ee7a4d
Sayan3sarkar <sayansarkar333@gmail.com>
2019-09-04 15:15:30 +0530

    Added code to remove unused interceptors.

--- burger/src/hoc/withErrorHandler/withErrorHandler.js
+++ burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -11,13 +11,19 @@ const withErrorHandler = (WrappedComponent, axios)=>{
 		}
 
 		componentWillMount(){
-			axios.interceptors.request.use(req=>{
+			this.reqInterceptor = axios.interceptors.request.use(req=>{
 				this.setState({error: null})
 				return req
 			})
-			axios.interceptors.response.use(res => res, error =>{
+			this.resInterceptor = axios.interceptors.response.use(res => res, error =>{
 				this.setState({error: error})
 			})
+		}
+
+		componentWillUnmount(){
+			//console.log('Will unmount', this.reqInterceptor, this.resInterceptor)
+			axios.interceptors.request.eject(this.reqInterceptor)
+			axios.interceptors.response.eject(this.resInterceptor)
 		}
 
 		errorConfirmedHandler= ()=>{
