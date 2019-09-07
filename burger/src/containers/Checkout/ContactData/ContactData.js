037e4c4e8bd18eeec30a50162693618255fdfc32
Sayan3sarkar <sayansarkar333@gmail.com>
2019-09-07 11:45:55 +0530

    Changed min and max zipcode length to 6 from 5

--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -60,8 +60,8 @@ class ContactData extends Component{
 		            	value: '',
 		            	validation: {
 		            		required: true,
-		            		minLength: 5,
-		            		maxLength: 5
+		            		minLength: 6,
+		            		maxLength: 6
 		            	},
 		            	valid: false,
 		            	touched: false,
